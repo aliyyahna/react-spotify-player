80ac2ed7a279b5aa1233a2a33ca39212366bd050
aliyyahna <33626085+aliyyahna@users.noreply.github.com>
2019-12-04 01:03:15 +0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+export const clientId = "93a1678b5e0d4dc189ade6bfd72c6d6e";
+export const redirectUri = "http://localhost:3000/";
+export const scopes = [
+  "user-top-read",
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
